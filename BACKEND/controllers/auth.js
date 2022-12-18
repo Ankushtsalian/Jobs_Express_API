242bbe2147e17c14ccd45d872c7c739f6920aa5e
Ankush <ankushtsalian@gmail.com>
2022-12-18 19:32:48 +0530

    init controller creation

---
+++ BACKEND/controllers/auth.js
@@ -0,0 +1,9 @@
+const register = async (req, res) => {
+  res.send("reg user");
+};
+
+const login = async (req, res) => {
+  res.send("reg user");
+};
+
+module.exports = { register, login };
---
+++ BACKEND/controllers/jobs.js
@@ -0,0 +1,27 @@
+const getAllJobs = async (req, res) => {
+  res.send("reg user");
+};
+
+const getJob = async (req, res) => {
+  res.send("reg user");
+};
+
+const createJob = async (req, res) => {
+  res.send("reg user");
+};
+
+const updateJob = async (req, res) => {
+  res.send("reg user");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("reg user");
+};
+
+module.exports = {
+  getAllJobs,
+  createJob,
+  deleteJob,
+  updateJob,
+  getJob,
+};
