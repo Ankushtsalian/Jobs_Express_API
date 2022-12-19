605a4a0612d66e08dc036dc690208eef57f11781
Ankush <ankushtsalian@gmail.com>
2022-12-19 17:51:24 +0530

    mongoose methods for user token

--- BACKEND/controllers/auth.js
+++ BACKEND/controllers/auth.js
@@ -1,19 +1,10 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const userToken = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SERCRET,
-    {
-      expiresIn: "30d",
-    }
-  );
-  res
-    .status(StatusCodes.CREATED)
-    .json({ user: { name: user.name }, token: userToken });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -28,5 +30,16 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.createJWT = function () {
+  const userToken = jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SERCRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  return userToken;
+};
 
 module.exports = mongoose.model("User", UserSchema);
