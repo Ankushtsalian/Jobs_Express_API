de6a0a6004d5f8b45c6e49df5d6ace10af73dd19
Ankush <ankushtsalian@gmail.com>
2022-12-24 19:34:42 +0530

    EXTRA SECURITY PACKAGES

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,8 +3,15 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-var cors = require("cors");
+//Extra Security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+// Auth Middleware
 const authenticateUser = require("./middleware/authentication");
+
 //routers
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
@@ -13,9 +20,20 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
-app.use(cors());
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
